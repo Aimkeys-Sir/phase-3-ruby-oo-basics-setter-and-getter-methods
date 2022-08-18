07955e47ce30877ca8be753020f57e0ce1db09fe
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-18 10:53:08 +0300

    commit tree 6b1c39f73cb2c63ded5c779cb2f01a436e64de01

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    def name=(_name)
+        @name=_name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(_job)
+        @job=_job
+    end
+
+    def job
+        @job
+    end
 end
